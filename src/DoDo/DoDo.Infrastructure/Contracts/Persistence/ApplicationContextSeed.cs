6ed4182d17fc51d83d88e12436a4536585d9536b
Erdi Demir <erdi.demir@icloud.com>
2022-02-13 13:09:18 +0300

    Seed Role Added

--- src/DoDo/DoDo.Infrastructure/Contracts/Persistence/ApplicationContextSeed.cs
+++ src/DoDo/DoDo.Infrastructure/Contracts/Persistence/ApplicationContextSeed.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using DoDo.Domain.Entities.Authentications;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +12,18 @@ namespace DoDo.Infrastructure.Contracts.Persistence
     {
         public static async Task SeedAsync(ApplicationContext applicationContext, ILogger<ApplicationContextSeed> logger)
         {
-           
+            if (!applicationContext.Roles.Any())
+            {
+                applicationContext.Roles.AddRange(new List<Role>
+                {
+                    new Role{ Name="Administrator"},
+                    new Role{ Name="Company"},
+                    new Role{ Name="Jobber"}
+
+                });
+                await applicationContext.SaveChangesAsync();
+                logger.LogInformation("Seed database associated with context {DbContextName}", typeof(ApplicationContext).Name);
+            }
         }
     }
 }
